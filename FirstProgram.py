4d149fc8eb47df16c1dd97acc366d1c0dcccb1d7
Tanvideshmukh14 <tanvidesh8@mail.com>
2026-01-07 14:55:54 +0530

    basics

---
+++ FirstProgram.py
@@ -0,0 +1,38 @@
+# print statements
+print("Hello World")
+print("Tanvi is my name","My age is 22")
+print(23)
+print(35+15) 
+
+# variable = value
+name = "Tanvi"
+age = 22
+price = 25.99
+
+print(name)
+print("my name is", name)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# Integers +ve -ve 0
+# String   "tanvi" 'tanvi' '''tanvi'''
+# Float    decimal values
+# Boolean  True False
+# None 
+
+age = 22
+old = False
+a = None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+# Keywprds in python
+# and  as assert break class continue def del elif else
+# except finally False for from global if Import in is lamda nonlocal
+# None not or pass raise return True try with while yield
+
+# python is case sensitive Apple and apple is different
---
+++ Operators.py
@@ -0,0 +1,20 @@
+a = 2
+b = 5
+sum = a + b
+print(sum)
+
+# single line comment
+"""multi-line 
+comment"""
+
+# operator
+# arithmetic (+ - * % **)
+# relational ( == != > < >= <=)
+# assignment (= += -= *= /= %= **=)
+# Logical (not and or)
+
+diff = b - a
+div = b / a
+mul = b * a
+power = b ** a
+print(power)
